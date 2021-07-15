556dd41a2f86e04e49e2df172d602c549d028928
Kelvin Ong <kelvinong.toronto@yahoo.ca>
2021-07-15 16:54:41 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# COMP229 Project
+
+## Welcome to the Project - the Modern Survey Site
+
+please use **`npm install`** to install project dependencies
---
+++ server/routes/index.js
@@ -0,0 +1,17 @@
+// modules required for routing
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// define the game model
+let survey = require('../models/surveys');
+
+/* GET home page. wildcard */
+router.get('/', (req, res, next) => {
+  res.render('content/index', {
+    title: 'Home',
+    surveys: ''
+   });
+});
+
+module.exports = router;
